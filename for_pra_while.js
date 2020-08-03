ff05a6231898a7a0290cab35a846abda6feda8fc
danielemaraschin <danikmaraschin@gmail.com>
2020-08-03 15:38:28 +0200

    comparative between FOR and While

---
+++ for_pra_while.js
@@ -0,0 +1,16 @@
+// primeiro em for como foi dado o exercicio
+//for( var i = 0; i < 10; i++ ) {
+//    console.log( "O resultado é " + (2 * i) );
+//}
+
+
+// depois em while, o que foi pedido:
+
+var i = 0;
+
+while (i<10){
+console.log("o resultado é " + (2*i) );
+i = i +1;
+}
+
+
