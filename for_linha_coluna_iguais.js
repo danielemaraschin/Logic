073de3d856821c338ee24e64543e8985540f0068
danielemaraschin <danikmaraschin@gmail.com>
2020-08-14 10:04:46 +0200

    working with for bimentional

---
+++ for_linha_coluna_iguais.js
@@ -0,0 +1,10 @@
+var resultado = "*";
+var pulaLinha = "\n";
+
+for (var linha = 1; linha <= 3; linha++) {
+    for (var coluna = 1; coluna <= 10; coluna++) {
+        resultado = resultado + "\n";
+    }
+    
+}
+console.log(resultado + pulaLinha);
