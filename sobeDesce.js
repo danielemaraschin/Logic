55c52f502c420976cafcb833afcbc656f0517ae6
danielemaraschin <danikmaraschin@gmail.com>
2020-08-18 10:01:00 +0200

    working with for and many vars

--- sobeDesce.js
+++ sobeDesce.js
@@ -1,13 +1,12 @@
-// var retorno = "";
-// var numeroColunas = 1;
-// var maximoDeColunas = 5;
-// var numeroEstrelinhas = maximoDeColunas - 1;
-// var quantidadeMaximaDeLinhas = maximoDeColunas * 2;
+var retorno = "";
+var maximoDeColunas = 10;
+var numeroEstrelinhas = maximoDeColunas - 1;
+var quantidadeMaximaDeLinhas = maximoDeColunas * 2;
 
 var retorno = "";
 var numeroEstrelinhas = 1;
 
-for (var linha = 0; linha < 11; linha++) {
+for (var linha = 0; linha <= quantidadeMaximaDeLinhas; linha++) {
     for (var coluna = 0; coluna < numeroEstrelinhas; coluna++) {
         retorno = retorno + "*";
     }
@@ -15,7 +14,7 @@ for (var linha = 0; linha < 11; linha++) {
     retorno = retorno + "\n";
     
 
-    if( linha < 5 ){
+    if( linha < maximoDeColunas ){
         numeroEstrelinhas++;
     }else{
         numeroEstrelinhas--;
@@ -23,23 +22,3 @@ for (var linha = 0; linha < 11; linha++) {
 }
 
 console.log(retorno);
-
-
-
-// for (var linha = 1; linha < quantidadeMaximaDeLinhas; linha++) {
-//     for (var coluna = 1; coluna < maximoDeColunas ; coluna++) {
-//         if (numeroColunas >= numeroEstrelinhas) {
-//             retorno = retorno + "*";
-//         } else {
-//             retorno = retorno + " ";
-
-//         }
-//     }
-//     if( linha < maximoDeColunas ){
-//         numeroEstrelinhas++;
-//     }else{
-//         numeroEstrelinhas--
-//     }
-//     retorno = retorno + "\n";
-//     console.log(retorno);
-// }
